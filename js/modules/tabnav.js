abcefcb2eba9b2e672b57d5b97f18f755b6985b2
Maristela Bispo <maristelabispon@gmail.com>
2022-07-26 20:24:59 -0300

    tabnav transformado em class

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,25 +1,33 @@
-export default function initTabNav() {
-  const animaisTexto = document.querySelectorAll(
-    '[data-tab="content"] section',
-  );
-  const animaisFoto = document.querySelectorAll('[data-tab="menu"] li');
-
-  function desaparecerTexto(index) {
-    animaisTexto.forEach((animal) => {
-      animal.classList.remove('ativo');
-    });
-    const direcao = animaisTexto[index].dataset.anime;
-
-    animaisTexto[index].classList.add('ativo', direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.animaisFoto = document.querySelectorAll(menu);
+    this.animaisTexto = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if (animaisTexto.length && animaisFoto) {
-    animaisTexto[0].classList.add('ativo');
+  desaparecerTexto(index) {
+    this.animaisTexto.forEach((animal) => {
+      animal.classList.remove(this.activeClass);
+    });
+    const direcao = this.animaisTexto[index].dataset.anime;
 
-    animaisFoto.forEach((animal, index) => {
+    this.animaisTexto[index].classList.add(this.activeClass, direcao);
+  }
+
+  //Adiciona os eventos
+  addTabNavEvent() {
+    this.animaisFoto.forEach((animal, index) => {
       animal.addEventListener('click', () => {
-        desaparecerTexto(index);
+        this.desaparecerTexto(index);
       });
     });
+  }
+
+  init() {
+    if (this.animaisTexto.length && this.animaisFoto.length) {
+      //ativar primeiro item
+      this.desaparecerTexto(0);
+      this.addTabNavEvent();
+    }
   }
 }
